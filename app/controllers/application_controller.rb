036b4b29b348afc0ef255c03edd327172e4a5ef5
remote-torrent-6747 <abbyc@sky.com>
2021-04-17 09:51:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    erb :new
   end
+
+  post '/newarticle' do
+    title = params[:title]
+    content = params[:content]
+    new_article = Article.create(title: title, content: content)
+    redirect "/articles/#{new_article.id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.where("id = ?", params[:id])
+    erb :show
+  end
+
 end
